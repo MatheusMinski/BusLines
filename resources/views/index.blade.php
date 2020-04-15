1a026fed55cd022cbbdf8ead68beb4aea801373b
Matheus Minski dos Santos <minskisantos@hotmail.com>
2020-04-15 11:00:34 -0300

    Test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('conteudo')
 <div class="text-center">
-    <h2>Seja bem vindo!</h2>
+    <h2>Seja bem vindo! Teste de Update</h2>
     <h4>Esse site tem como objetivo divulgar os dados das rotas públicas de ônibus da cidade.</h4>
     <h4>Utilizando o menu superior podemos acessar essas informações, vá em frente!</h4>
 
